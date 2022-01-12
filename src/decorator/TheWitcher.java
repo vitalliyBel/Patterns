3f31d67a3b1cbf2af5d5098cdd16858b31e15ec8
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-12 23:28:26 +0300

    created new branch and public interface TheWitcher in new package decorator

---
+++ src/decorator/TheWitcher.java
@@ -0,0 +1,11 @@
+package decorator;
+
+public interface TheWitcher {
+
+        String whatDoesTheWitcherLookLike();
+        String signsOfTheWitcher();
+
+        int basicStrength();
+        int basicDexterity();
+    }
+
