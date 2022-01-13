e3c8da4d1840a9e170650cd9eb180adb820798c0
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-13 22:44:43 +0300

    Added some more soul to the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,10 +16,11 @@ public class Main {
         System.out.println("The physical strength of a novice witcher - " + noviceWitcher.basicStrength());
         System.out.println("The dexterity of a novice witcher - " + noviceWitcher.basicDexterity());
         System.out.println(form);
-
+        System.out.println("Twenty years have passed, the Witcher has gone through many trials,\n" +
+                "gained experience, strength and knowledge.");
+        System.out.println(form);
 
         TheWitcher witcherExp = new TheWitcherExperienceDec(noviceWitcher);
-
 
         System.out.println("What does an experienced witcher look like - " + witcherExp.whatDoesTheWitcherLookLike());
         System.out.println("What signs does an experienced witcher know how to use: \n " + witcherExp.signsOfTheWitcher());
