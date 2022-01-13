e4882ebe91cedfd157d1f44233d2128c46d6a224
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-13 23:16:20 +0300

    Started working on the adapter pattern

---
+++ src/adapter/Love.java
@@ -0,0 +1,5 @@
+package adapter;
+
+public interface Love {
+    void cupidsArrow();
+}
---
+++ src/adapter/MenWithBadTemper.java
@@ -0,0 +1,13 @@
+package adapter;
+
+public class MenWithBadTemper {
+
+    public void  fellInLove(){
+        System.out.println("Влюбился");
+    }
+
+    public void  gotBetter(){
+        System.out.println();
+    }
+
+}
