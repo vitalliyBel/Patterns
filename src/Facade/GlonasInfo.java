cfffd9611f21db31021d2796200671b49749840f
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-14 20:59:14 +0300

    Created a package for an example using the Facade pattern

---
+++ src/Facade/GlonasInfo.java
@@ -0,0 +1,10 @@
+package Facade;
+
+public class GlonasInfo {
+
+    public void downloadInfoFromSatellite(){
+        System.out.println("Получение информации со спутника......");
+        System.out.println("Информация получена");
+    }
+}
+
---
+++ src/Facade/QuickRouteToPizza.java
@@ -0,0 +1,8 @@
+package Facade;
+
+public class QuickRouteToPizza {
+
+    public void  creatingRoute (){
+        System.out.println("Выбран самый быстрый маршрут до ближайшей пиццерии");
+    }
+}
