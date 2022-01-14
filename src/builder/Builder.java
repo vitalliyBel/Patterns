045c3eeb81671f2c5834bf97787b76c27d608ece
AleksandrChernov1988 <Aleksander88Cherhov@yandex.by>
2022-01-14 22:33:39 +0200

    add builder

---
+++ src/builder/Builder.java
@@ -0,0 +1,8 @@
+package builder;
+
+public interface Builder {
+    void setTypeDrink(DrinkType typeCoffee);
+    void setCupVolume(Volume volume);
+    void setCream(Cream cream);
+    void setSugar(Sugar sugar);
+}
