46f2721267a3e5799d7d70b07a7d0976c7cb224a
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-14 23:18:19 +0300

    Started making a bridge pattern

---
+++ src/bridge/Black.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class Black implements Color{
+    @Override
+    public void paint() {
+        System.out.println("Красим в черный цвет");
+    }
+}
---
+++ src/bridge/Red.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public class Red implements Color {
+
+    @Override
+    public void paint() {
+        System.out.println("Красим в красный цвет");
+    }
+}
