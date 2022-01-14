358edc636073dfef1ebe007e12f3368639ba8987
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-14 23:27:30 +0300

    Started paint furniture

--- src/bridge/Furniture.java
+++ src/bridge/Furniture.java
@@ -1,4 +1,15 @@
 package bridge;
 
 public abstract class  Furniture {
+
+    protected Color color;
+
+    protected  Furniture(Color color){
+        this.color = color;
+    }
+
+    public abstract void paint();
+
+
+
 }
---
+++ src/bridge/Table.java
@@ -0,0 +1,12 @@
+package bridge;
+
+public class Table  extends Furniture{
+    protected Table(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void paint() {
+
+    }
+}
